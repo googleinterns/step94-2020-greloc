5aa1ba391b8849d4a437e73c2af09d1d75e52d03
Nico <capparellinicog123@gmail.com>
2020-07-01 19:14:22 +0000

    Implement listing info component, -Implement connecting listing and listing info components together, -Implement route to office from selected listing

--- vue-dash/src/components/sub-components/map-subpage/Listings.vue
+++ vue-dash/src/components/sub-components/map-subpage/Listings.vue
@@ -1,23 +1,56 @@
 <template>
-  <div id="listings-component">
-    <Listing 
+  <v-list 
+    id="listings-component"
+  >
+    <v-list-item-group v-model="selectedListing">
+      <v-list-item
         v-for="(listing, index) in listings"
         :key="index"
-        :listingInfo="listing"        
-    />
-  </div>
+        @click="onUserSelectsListing(listing)"
+      >
+        <Listing :listingInfo="listing"/>
+        <v-divider></v-divider>
+      </v-list-item>
+    </v-list-item-group>    
+  </v-list>
 </template>
 
 <script>
 import Listing from './Listing.vue'
+import { EVENTS } from '../../../utils/constants.js'
 
 export default {
   name: 'Listings',
+  created () {    
+    this.$root.$on(EVENTS.listingDeselected, () => {
+      this.clearSelection();
+    });    
+  },
+
   components: {
     Listing
   },
   props: {
     listings: Array
+  },
+
+  data: () => ({
+    selectedListing: null
+  }),
+
+  methods: {    
+    onUserSelectsListing: function(listing) {
+      if (this.selectedListing != null) {
+        this.clearSelection();
+      } else {
+        this.$root.$emit(EVENTS.listingSelected, listing);
+      }
+    },
+
+    clearSelection: function () {
+      this.selectedListing = null;
+      this.$root.$emit(EVENTS.listingSelected, null);
+    }
   }
 }
 </script>
@@ -26,5 +59,7 @@ export default {
   #listings-component {
     height: 100%;
     width: 100%;
+
+    background-color: transparent;
   }
 </style>
--- vue-dash/src/utils/constants.js
+++ vue-dash/src/utils/constants.js
@@ -2,6 +2,13 @@
 // const WEBSITE_URL = "http://capparelli-step-2020.appspot.com"
 const WEBSITE_URL = "https://8080-b83fc153-d2cf-481d-a321-9342cdf80f21.us-east1.cloudshell.dev";
 
+const EVENTS = {
+  officeChanged: "officeChanged",
+  newListings: "newListings",
+  listingSelected: "listingSelected",
+  listingDeselected: "listingDeselected"
+}
+
 const OFFICES = [
   { 
     name: "Sunnyvale", 
@@ -22,4 +29,4 @@ const OFFICES = [
 ]
 
 
-export { WEBSITE_URL, OFFICES };
+export { EVENTS, WEBSITE_URL, OFFICES };
