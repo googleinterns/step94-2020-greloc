b5adfebfce7d1a5c0251f548bf52223129fe1ad9
Erik <erikmej01@gmail.com>
2020-07-01 08:03:56 +0000

    Update subpage to incoperate form component

--- vue-dash/src/components/MyListingsSubpage.vue
+++ vue-dash/src/components/MyListingsSubpage.vue
@@ -1,23 +1,48 @@
 <template>
-    <div class="subpage" id = "my-listings-subpage">
-        <h2>My Listings</h2>
-        <!-- Listings Container Component -->
-    </div>
-    
+  <div class="subpage" id="my-listings-subpage">
+    <MyListingsContainer class="property-list"/>
+      <ListingForm class="form"/>
+  </div>
 </template>
 
 <script>
+import ListingForm from './sub-components/myListings-subpage/EditListing.vue'
+import MyListingsContainer from './sub-components/myListings-subpage/MyListingsContainer.vue'
 
 export default {
-  name: 'Template',
+  name: 'MyListingsSubpage',
   props: {
     msg: String
   },
   components: {
+    ListingForm,
+    MyListingsContainer
   }
 }
 </script>
 
 <style scoped>
+.property-list {
+  min-width: 550px;
+  height: 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  border-right: 1px solid #dbdbdb;
+  padding: 16px;
+}
 
+.form {
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-end;
+  flex-direction: column;
+  padding: 30px;
+}
+
+#my-listings-subpage {
+  display: flex; 
+  align-items: flex-start;
+  justify-content: flex-start;
+}
 </style>
