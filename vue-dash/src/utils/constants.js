4a9ab63c8ccab6c9b32b61269cc2286226c2046d
Nico <capparellinicog123@gmail.com>
2020-07-29 10:04:10 +0000

    - Implement new way to store API keys -Remove old test code

--- vue-dash/src/utils/constants.js
+++ vue-dash/src/utils/constants.js
@@ -1,6 +1,6 @@
  
-// const WEBSITE_URL = "https://greloc.uc.r.appspot.com"
-const WEBSITE_URL = "https://8080-b83fc153-d2cf-481d-a321-9342cdf80f21.us-east1.cloudshell.dev";
+const WEBSITE_URL = "https://greloc.uc.r.appspot.com"
+// const WEBSITE_URL = "https://8080-b83fc153-d2cf-481d-a321-9342cdf80f21.us-east1.cloudshell.dev";
 
 const EVENTS = {
   officeChanged: "officeChanged",
--- vue-dash/src/utils/gmaps.js
+++ vue-dash/src/utils/gmaps.js
@@ -2,7 +2,7 @@
 
 // Your personal API key.
 // Get it here: https://console.cloud.google.com/google/maps-apis
-const API_KEY = 'AIzaSyCRG4mbpl4ZOXTmB8xACmqAcnnDSDdLJFU';
+const API_KEY = '';
 const CALLBACK_NAME = 'gmapsCallback';
 
 let initialized = !!window.google;
