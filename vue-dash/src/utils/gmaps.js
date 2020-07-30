0264c00d5e6b61ca635c17404b70f53522b0b392
Toni Carr <toni.carr0905@gmail.com>
2020-07-30 14:21:51 +0000

    add files

--- vue-dash/src/utils/gmaps.js
+++ vue-dash/src/utils/gmaps.js
@@ -2,7 +2,7 @@
 
 // Your personal API key.
 // Get it here: https://console.cloud.google.com/google/maps-apis
-const API_KEY = '';
+const API_KEY = 'AIzaSyBNACaFxumY4HYDoNrJDUcCny4cpONKmK8';
 const CALLBACK_NAME = 'gmapsCallback';
 
 let initialized = !!window.google;
